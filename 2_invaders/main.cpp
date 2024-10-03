74e9d2fcc93f1072ab0bc2f7cc8bd337756c88e1
40628520 <40628520@live.napier.ac.uk>
2024-10-03 12:43:18 +0100

    Pong updated code

--- 2_invaders/main.cpp
+++ 2_invaders/main.cpp
@@ -1,20 +1,33 @@
 #include <SFML/Graphics.hpp>
 
-int main(){
-  sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+using namespace sf;
 
-  while (window.isOpen()){
-      sf::Event event;
-      while (window.pollEvent(event)){
-      if (event.type == sf::Event::Closed){
-        window.close();
-      }
+const int gameWidth = 800;
+const int gameHeight = 600;
+
+// Function Prototypes
+void Load();
+void Update(RenderWindow& window);
+void Render(RenderWindow& window);
+
+int main() {
+    // Initialize the window
+    RenderWindow window(VideoMode(gameWidth, gameHeight), "SPACEINVADERS_GAME");
+
+    // Load initial game state
+    Load();
+
+    // Main game loop
+    while (window.isOpen()) {
+        window.clear();
+
+
+        Update(window);
+
+        Render(window);
+
+        window.display();
     }
-    window.clear();
-    window.draw(shape);
-    window.display();
-  }
-  return 0;
+
+    return 0;
 }
